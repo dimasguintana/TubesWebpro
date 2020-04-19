858f142e3984f572eaf533068c65e67368168341
dimasguintana <dguint@outlook.com>
2020-04-19 22:10:03 +0700

    Pasien_Model

---
+++ application/models/Pasien_Model.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Pasien_Model extends CI_Model {
+
+	
+
+}
+
+/* End of file Pasien_Model.php */
+/* Location: ./application/models/Pasien_Model.php */
