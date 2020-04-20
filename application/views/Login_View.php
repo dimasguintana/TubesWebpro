e0aa90e0c6ce6873bac36d5a91d2cfd5bbce15e9
rizqichoirulfikri <42829113+rizqichoirulfikri@users.noreply.github.com>
2020-04-20 23:58:45 +0700

    welcome

---
+++ application/views/Login_View.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: #000000;">
+  <a class="navbar-brand" href="#">Health Home</a>
+  <!-- <img src="<?= base_url('assets/healthhome.png') ?>" alt="logo"> -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+</nav>
+<?= $this->session->flashdata('message'); ?>
+<form method="POST" action="<?php echo site_url('Welcome/login') ?>">
+  <div class="form-group">
+    <label for="username">username</label>
+    <input type="text" class="form-control" id="username" name="username" placeholder="username">
+  </div>
+  <div class="form-group">
+    <label for="password">Password</label>
+    <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+  </div>
+  <button type="submit" class="btn btn-primary">Login</button>
+</form>
+<?= $this->session->userdata('username')?>
