41b94662acc78517cd5b521ccc832a205bbc0731
dimasguintana <dguint@outlook.com>
2020-04-20 23:06:07 +0700

    login di Welcome

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -11,12 +11,82 @@ class Welcome extends CI_Controller {
 
 	public function index()
 	{
-		$content['title'] = 'Pasien';
-		$this->load->view('welcome_message', $content);
+		$content['main_view'] = 'Login_View';
+		$content['title'] = 'Login';
+		$this->load->view('Halaman', $content);
 	}
 
-	public function allPasien()
+	public function login()
 	{
-		echo json_encode($this->Pasien_Model->getPasien());
+		$this->form_validation->set_rules('username', 'Username', 'trim|required');
+		$this->form_validation->set_rules('password', 'Password', 'trim|required|min_length[3]');
+
+		$cdata['main_view'] = 'Login_View';
+		$data['title'] = 'Login';
+
+		if ($this->form_validation->run() == FALSE) {
+			$this->load->view('Halaman', $data);
+		} else {
+			$this->input->post('username');
+			$this->input->post('password');
+
+			$peran;
+			$query1 = $this->db->get_where('pasien', ['username' => $username])->row_array();
+			if ($query1->num_rows() > 0) {
+				$peran = 'pasien';
+			}
+
+			$query2 = $this->db->get_where('dokter', ['username' => $username])->row_array();
+			if ($query2->num_rows() > 0) {
+				$peran = 'dokter';
+			}
+
+			$query3 = $this->db->get_where('admin', ['username' => $username])->row_array();
+			if ($query3->num_rows() > 0) {
+				$peran = 'admin';
+			}
+
+			//Jika username valid di pasien
+			if ($peran == 'pasien') {
+
+				if (password_verify($password, $query1['password'])) {
+					$data = [
+						'username' => $query1['username'],
+						'nama' => $query1['username'],
+						'peran' => $peran
+					];
+				} else {
+					$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Password Invalid</div>');
+					redirect('Welcome/login');
+				}
+				//Jika username valid di dokter
+			} else if ($peran == 'dokter') {
+				if (password_verify($password, $query2['password'])) {
+					$data = [
+						'username' => $query2['username'],
+						'nama' => $query2['username'],
+						'peran' => $peran
+					];
+				} else {
+					$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Password Invalid</div>');
+					redirect('Welcome/login');
+				}
+				//Jika username valid di admin
+			} else if ($peran == 'admin') {
+				if (password_verify($password, $query3['password'])) {
+					$data = [
+						'username' => $query3['username'],
+						'nama' => $query3['username'],
+						'peran' => $peran
+					];
+				} else {
+					$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Password Invalid</div>');
+					redirect('Welcome/login');
+				}
+			} else {
+				$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Username Invalid</div>');
+				redirect('Welcome/login');
+			}
+		}
 	}
 }
