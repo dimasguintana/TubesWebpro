d9c47979339ba6027aee2f46cb724abbf815e514
rizqichoirulfikri <42829113+rizqichoirulfikri@users.noreply.github.com>
2020-04-21 17:40:50 +0700

    nambah gambar

--- application/views/template/atas.php
+++ application/views/template/atas.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-expand-sm navbar-dark" style= 'background-color: ;'>
-  <img src="<?= base_url('assets/healthhome.png') ?>" alt="logo" width=50 height=50>
-  <a class="navbar-brand" href="#" style= 'color: #000000'> Health Home</a>
+<nav class="navbar navbar-expand-sm navbar-dark" style= 'background-color: #1A2E35;'>
+  <img src="<?= base_url('assets/logo-2.jpg') ?>" alt="logo" width=50 height=50>
+  <a class="navbar-brand" href="#" style= 'color: #FFFFFF'> Health Home</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
