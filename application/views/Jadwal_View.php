97bba8e958bdad1a7b5a8621a03e616876cb2e09
saniyahnf <42828908+saniyahnf@users.noreply.github.com>
2020-04-22 19:28:10 +0700

    Update Jadwal_View.php

--- application/views/Jadwal_View.php
+++ application/views/Jadwal_View.php
@@ -1,12 +1,14 @@
-<!-- GET SEMUA DATA JADWAL -->
-  <div class="py-5">
+<div class="py-5">
     <h1 class="text-center"><?= $title ?></h1>
-    <div class="table-responsive container">                <!-- id table -->
+    <div class="table-responsive container">
+    <div class="d-flex justify-content-end">
+    </div>                                                        <!-- id table -->
       <table class="table table-dark table-hover table-bordered" id="dataJadwal" style="width: 100%">
         <thead>
           <tr>
-            <th>Username Pasien</th>
+            <th>Username Pasien </th>
             <th>Username Dokter</th>
+            <th>Username Admin</th>
             <th>Tanggal</th>
             <th>Jam</th>
             <th>Ruangan</th>
@@ -15,7 +17,7 @@
       </table>
     </div>
   </div>
-<script type="text/javascript">
+  <script type="text/javascript">
   $(document).ready(function() {
     let table = $('#dataJadwal').dataTable({
       "searching": false,
@@ -28,31 +30,27 @@
         "type": "GET",
         "dataSrc": ""
       },
-      "columns": [{
-          "data": "username_pasien"
+
+      "columns": [
+        {
+          "data": "username Pasien"
         },
         {
-          "data": "username_dokter"
+          "data": "Username Dokter"
         },
         {
-          "data": "tanggal"
+          "data": "Username Admin"
         },
         {
-          "data": "jam"
+          "data": "Tanggal"
         },
         {
-          "data": "ruangan"
+          "data": "Jam"
+        },
+        {
+          "data": "Ruangan"
         }
       ]
   });
-//   $.ajax({
-//     url:"<?= site_url('Jadwal/allJadwal')?>", 
-//     type: "GET",
-//     async : true,
-//     dataType : "JSON",
-//     success : function(data){
-//       console.log(data);
-//     }
-//   })
-  });
+});
 </script>
