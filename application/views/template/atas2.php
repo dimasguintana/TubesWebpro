cd2e910e1846c3e258160445da56962d57c3ae9e
rizqichoirulfikri <42829113+rizqichoirulfikri@users.noreply.github.com>
2020-04-22 21:57:20 +0700

    Merge pull request #37 from dimasguintana/bendi
    Header atas2

--- application/views/template/atas2.php
+++ application/views/template/atas2.php
@@ -4,7 +4,6 @@
    	<a class="navbar-brand" href="#" style= 'color: #FFFFFF;  margin-left: 10px'> Health Home</a>
    	<a class="nav-item nav-link" href="<?= site_url('Pasien') ?>" style= 'color: #FFFFFF;  margin-left: 10px'>Pasien <span class="sr-only">(current)</span></a>
    	<a class="nav-item nav-link" href="<?= site_url('Dokter') ?>" style= 'color: #FFFFFF;  margin-left: 10px'>Dokter</a>
-    <a class="nav-item nav-link" href="#" style= 'color: #FFFFFF;  margin-left: 10px'>Jadwal</a>
    </div>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
