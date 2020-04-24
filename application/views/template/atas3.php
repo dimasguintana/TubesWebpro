25549d204ba729cbe37e871a0913aab9a21f5afb
dimasguintana <dguint@outlook.com>
2020-04-24 16:14:15 +0700

    href Booking buat atas3, ganti main_view Pasien, tambah controller Booking

--- application/views/template/atas3.php
+++ application/views/template/atas3.php
@@ -2,8 +2,8 @@
 <nav class="navbar navbar-expand-sm navbar-dark" style= 'background-color: #1A2E35;'>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
   	<img src="<?= base_url('assets/logo-2.jpg') ?>" alt="logo" width=50 height=50 >
-   	<a class="navbar-brand" href="#" style= 'color: #FFFFFF;  margin-left: 10px'> Health Home</a>
-   	<a class="nav-item nav-link" href="#" style= 'color: #FFFFFF;  margin-left: 10px'>Booking <span class="sr-only">(current)</span></a>
+   	<a class="navbar-brand" href="<?= site_url('Pasien') ?>" style= 'color: #FFFFFF;  margin-left: 10px'> Health Home</a>
+   	<a class="nav-item nav-link" href="<?= site_url('Booking') ?>" style= 'color: #FFFFFF;  margin-left: 10px'>Booking <span class="sr-only">(current)</span></a>
    	<a class="nav-item nav-link" href="#" style= 'color: #FFFFFF;  margin-left: 10px'>Cari Jadwal</a>
   </div>
   <div class="d-flex justify-content-end">
